13436f88d59b388f2c3e6079f40b3750e8597c81
vadholovin <holyvad@gmail.com>
2022-06-06 16:22:33 +0300

    Start styling PaletteList

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,14 +1,40 @@
 import React from 'react';
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
 import MiniPalette from './MiniPalette';
 
 function PaletteList({ palettes }) {
   return (
-    <div>
-      <h1>React Colors</h1>
-      {palettes.map((palette, index) => (
-        <MiniPalette {...palette} key={index} />
-      ))}
-    </div>
+    <Box
+      sx={{
+        backgroundColor: 'blue',
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'flex-start'
+      }}
+    >
+      <Container maxWidth="md">
+        <Box
+          component="nav"
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center'
+          }}
+        >
+          <h1>React Colors</h1>
+          <span>Create Palette</span>
+        </Box>
+
+        <Grid container spacing={4}>
+          {palettes.map((palette, index) => (
+            <MiniPalette {...palette} key={index} />
+          ))}
+        </Grid>
+      </Container>
+    </Box>
   );
 }
 
